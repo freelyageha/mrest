614aec3a143e4449c81445ccaa13d334728a38b3
Clarke-lee <clarke.lee@daumkakao.com>
2015-06-21 16:38:57 +0900

    #2 display reservable room list at host show

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -5,7 +5,16 @@ class HostsController < ApplicationController
   end
 
   def show
+    checkin = params.fetch(:checkin, nil)
+    checkout = params.fetch(:checkout, nil)
     @host = Host.find(params[:id])
+
+    @rooms = Room.where(host: @host)
+    @rooms = if checkin.present? && checkout.present?
+                  checkin = checkin.to_date
+                  checkout = checkout.to_date
+                  @rooms.reservable(checkin, checkout)
+                end
   end
 
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,13 @@
 class Room < ActiveRecord::Base
   belongs_to :host
+  has_many :schedules
+
+  scope :reservable, -> (checkin, checkout) {
+    eager_load(:schedules)
+      .where("schedules.year >= #{checkin.year} AND schedules.year <= #{checkin.year}")
+      .where("schedules.month >= #{checkin.month} AND schedules.month <= #{checkout.month}")
+      .where("schedules.day >= #{checkin.day} AND schedules.day < #{checkout.day}")
+      .where(schedules: { reserved: false})
+  }
+
 end
