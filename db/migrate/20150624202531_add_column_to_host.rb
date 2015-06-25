909729e7d3009cf6512cf9976bfce9ed3a69f17b
Clarke-lee <clarke.lee@daumkakao.com>
2015-06-25 19:08:18 +0900

    #9 type three parser complete

--- db/migrate/20150624202531_add_column_to_host.rb
+++ db/migrate/20150624202531_add_column_to_host.rb
@@ -5,5 +5,8 @@ class AddColumnToHost < ActiveRecord::Migration
     add_column :hosts, :country, :string
     add_column :hosts, :latitude, :float
     add_column :hosts, :longitude, :float
+
+    add_column :hosts, :need_login, :boolean, default: false
+    add_column :hosts, :parsed, :boolean, default: false
   end
 end
